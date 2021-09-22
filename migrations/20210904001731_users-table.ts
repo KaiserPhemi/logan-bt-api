ed469417c753684d8c0d41da71b328ba08def791
KaiserPhemi <kaiser.phemi@protonmail.ch>
2021-09-22 22:11:51 +0100

    feature(users table)
    - modify database table

--- migrations/20210904001731_users-table.ts
+++ migrations/20210904001731_users-table.ts
@@ -10,10 +10,11 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("users_table", (table: any) => {
     table.increments("id").primary();
-    table.uuid("public_id").defaultTo(knex.raw("uuid_generate_v4()"));
     table.string("name").notNullable().unique();
     table.string("email").notNullable().unique();
     table.string("password").notNullable().unique();
+    table.boolean("is_email_verified").defaultTo(false);
+    table.boolean("is_deleted").defaultTo(false);
     table.timestamps(true, true);
   });
 }
