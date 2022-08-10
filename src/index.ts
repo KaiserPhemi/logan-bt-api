033d9547545ad96f3079351593a6acfc1f987b3a
KaiserPhemi <kaiser.phemi@protonmail.ch>
2022-08-10 23:12:39 +0100

    chore()
    - start afresh

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// third-party libraries
-import express from "express";
-
-// instantiate app
-const app = express();
-
-// start app on port
-app.listen(5000, () => console.log(`App running on port 5000`));
